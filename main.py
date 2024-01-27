4c16b7f7a1c391b09ec7a2baac85abe7db75a91b
Moin Uddin <lsfhv@protonmail.com>
2024-01-27 15:53:16 +0000

    add uniform noise

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-# {gender, applicantType, minBedSize, olderPersonAssessment, mobilityAssessemnt} -> {weeksToHouse}
-from random import randint
-
-# applicantType in {A, B, C} |-> {1,2,3}
-# olderPersonAssessment in {Urgent, General, Homeless} |-> {1,2,3}
-# mobilityAssessemnt in {Urgent, General, Homeless, Homeless / Priority} |-> {1,2,3,4}
+from random import randint, uniform
 
 genderToInteger = {'F':1, 'M':2}
 applicantTypeToInteger = {'A': 1, 'B': 2, 'C': 3}
 olderPersonAssessmentToInteger = {'Urgent': 1, 'General': 2, 'Homeless': 3, 'Reserve': 4}
 mobilityAssessemntToInteger = {'Urgent': 1, 'General': 2, 'Homeless': 3, 'Homeless / Priority': 4, 'Reserve': 5}
 
-p_male = 139304 / 279554
-p_female = 1 - p_male
 # Load the dataset
 data = pd.read_csv('sample.csv')
 
@@ -71,7 +63,8 @@ def generateData(n):
         for l,r in bounds:
             instance.append(randint(l,r))
         X.append(instance)
-        Y.append(y(instance))
+        Y.append(y(instance) * uniform(0.9, 1.1))
     return X,Y
 
-print(generateData(10))
+X,Y = generateData(1)
+print(X,Y)
