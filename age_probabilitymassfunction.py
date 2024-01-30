7effdc1962e3a867e68ab8fa5df871b13c39f3ca
Moin Uddin <lsfhv@protonmail.com>
2024-01-30 16:33:24 +0000

    get age from pmf

--- age_probabilitymassfunction.py
+++ age_probabilitymassfunction.py
@@ -12,9 +12,12 @@ ageRangeToPop = [[i[0], i[1] / totalPop] for i in ageRangeToPop]
 age_ranges = [i[0] for i in ageRangeToPop]
 
 probabilities = [i[1] for i in ageRangeToPop]
-# def getAgeRange(x):
 
 # return an age range from the pmf.
 def getAgeRange():
     return np.random.choice(age_ranges, p=probabilities)
 
+def getRandomAge():
+    ageRange = getAgeRange()
+    ageRange = ageRange.split('-')
+    return np.random.randint(int(ageRange[0]), int(ageRange[1]))
