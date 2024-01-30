ef03300840a6e737782f7b089f47deaef68f18f5
Moin Uddin <lsfhv@protonmail.com>
2024-01-30 12:57:39 +0000

    age

--- age_probabilitymassfunction.py
+++ age_probabilitymassfunction.py
@@ -6,3 +6,7 @@ ageRanges = data['age'].values
 pop = data['pop'].values
 
 ageRangeToPop = [i for i in map(lambda x,y: [x,y], ageRanges, pop) if int(i[0].split('-')[0]) >= 18]
+
+totalPop = sum([i[1] for i in ageRangeToPop])
+ageRangeToPop = [[i[0], i[1] / totalPop] for i in ageRangeToPop]
+
