b2160f2f48f9aeb4ce9f016cb3fba8716f25338e
Moin Uddin <lsfhv@protonmail.com>
2024-01-30 16:29:38 +0000

    generate ages from pmf based on 2019 stats

--- age_probabilitymassfunction.py
+++ age_probabilitymassfunction.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 data = pd.read_csv('age_stats.csv')
 
 ageRanges = data['age'].values
@@ -9,4 +9,12 @@ ageRangeToPop = [i for i in map(lambda x,y: [x,y], ageRanges, pop) if int(i[0].s
 
 totalPop = sum([i[1] for i in ageRangeToPop])
 ageRangeToPop = [[i[0], i[1] / totalPop] for i in ageRangeToPop]
+age_ranges = [i[0] for i in ageRangeToPop]
+
+probabilities = [i[1] for i in ageRangeToPop]
+# def getAgeRange(x):
+
+# return an age range from the pmf.
+def getAgeRange():
+    return np.random.choice(age_ranges, p=probabilities)
 
